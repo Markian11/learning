4844b292024a4b552eda5a5e61bc3ef90d49e868
markian bilyashevych <mbilyashe@gmail.com>
2022-12-03 20:33:15 +0000

    added program

--- test.py
+++ test.py
@@ -1,1 +1,7 @@
-print("hello world")
+a = int(input("write your number here:"))
+for i in range(2,round(a/2)+1):
+    if a % i == 0:
+     print(a,"its not a prime number")
+    break
+else: 
+    print(a,"its a prime number")
